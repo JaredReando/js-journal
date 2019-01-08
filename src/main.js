288d32f921df2852d3fb570e277c920fc055376c
Jared Reando <jaredreando@gmail.com>
2019-01-07 20:12:40 -0800

    Add more dependencies.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { Entry } from './journal';
 import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
   $('#journal-form').submit(function(event) {
--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-lodaer"
+        loader: "eslint-loader"
       }
     ]
   }
